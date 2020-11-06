a557552d7314a4c1bf9bc9ac5a05d75c0223a397
PAU2007 <74076486+PAU2007@users.noreply.github.com>
2020-11-06 22:47:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-Hola
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+
+const client = new Discord.Client();
+
+const PREFIX = "t/";
+
+
+
+client.login("4vNGEjx77K7hcS1K-KWy9Z7oPT8nnIjr");
+
+
+
+
+
+
+
